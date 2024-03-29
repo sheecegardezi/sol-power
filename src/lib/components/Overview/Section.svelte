2246729bc8c9eb396e7ef8e613bc91d4ff478dce
sipofnightshade <gregorymcmillan96@gmail.com>
2024-03-28 20:16:50 -0400

    updated-- Overview Section layout ✅

--- src/lib/components/Overview/Section.svelte
+++ src/lib/components/Overview/Section.svelte
@@ -2,11 +2,16 @@
 	import SubHeader from '../Typography/SubHeader.svelte';
 
 	export let title: string;
+	export let grid: boolean = false;
 </script>
 
 <div class="relative flex w-full flex-col gap-y-4">
 	<SubHeader {title} />
-	<div class="grid -space-y-12 sm:grid-cols-2 sm:gap-4 sm:space-y-0 md:grid-cols-3">
+	{#if grid}
+		<div class="grid -space-y-12 sm:grid-cols-2 sm:gap-4 sm:space-y-0 md:grid-cols-3">
+			<slot />
+		</div>
+	{:else}
 		<slot />
-	</div>
+	{/if}
 </div>
