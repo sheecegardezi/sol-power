96699928e993cf1b10da1e46966f54ad2d626918
sipofnightshade <gregorymcmillan96@gmail.com>
2024-03-31 00:20:07 -0400

    updated-- BillsModal 🛠

---
+++ src/lib/components/Bill/CalcRow.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let label: string;
+	export let result: number;
+	export let highlight: boolean = false;
+</script>
+
+<div
+	class="flex w-full gap-2 text-sm [&>*]:leading-none"
+	class:bg-primary={highlight}
+	class:text-primary-foreground={highlight}
+	class:py-1={highlight}
+>
+	<h3 class="text-nowrap font-bold uppercase tracking-wide">{label}</h3>
+	<div class="flex w-full items-end justify-between gap-1">
+		<p class="w-fit text-nowrap">
+			<slot />
+		</p>
+		<div class="mb-0.5 h-px w-full border-b border-dashed border-muted-foreground bg-muted" />
+
+		<p class="text-nowrap">${result.toFixed(2)}</p>
+	</div>
+</div>
--- src/lib/components/Modals/BillsModal.svelte
+++ src/lib/components/Modals/BillsModal.svelte
@@ -2,6 +2,7 @@
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { format, parse } from 'date-fns';
 	import Ellipsis from 'lucide-svelte/icons/ellipsis';
+	import CalcRow from '../Bill/CalcRow.svelte';
 
 	interface Bill {
 		date: string;
@@ -21,6 +22,22 @@
 		const formattedDate = format(date, 'MMMM yyyy');
 		return formattedDate;
 	}
+
+	const rate = 0.5;
+	const fRate = 0.59;
+	const vatRate = 0.16;
+
+	const amountDue = bill.amountDue / 100;
+	const amountPaid = bill.amountPaid / 100;
+	const days = bill.numDays;
+	const unitsUsed = bill.unitsUsed;
+	const date = formatDate(bill.date);
+
+	$: vatAmt = bill.unitsUsed - 150 ? bill.unitsUsed - 150 : 0;
+	$: vatRes = vatAmt ? vatAmt * vatRate * rate : 0;
+
+	$: energyFees = unitsUsed * rate;
+	$: fuelSurcharge = unitsUsed * fRate;
 </script>
 
 <Dialog.Root>
@@ -31,11 +48,29 @@
 	</Dialog.Trigger>
 	<Dialog.Content>
 		<Dialog.Header>
-			<Dialog.Title>Bill Breakdown - {formatDate(bill.date)}</Dialog.Title>
-			<Dialog.Description>
-				This action cannot be undone. This will permanently delete your account and remove your data
-				from our servers.
-			</Dialog.Description>
+			<Dialog.Title>Bill Breakdown - {date}</Dialog.Title>
+			<Dialog.Description>See how your charges were calculated!</Dialog.Description>
 		</Dialog.Header>
+
+		<div class="flex w-full flex-col gap-2">
+			<h2 class="font-semibold text-muted-foreground">Current fees</h2>
+			<CalcRow label="Energy usage" result={energyFees}
+				>{unitsUsed.toFixed(1)} kWh x ${rate}</CalcRow
+			>
+			<CalcRow label="VAT" result={vatRes}>{vatAmt.toFixed(1)} kWh x {vatRate}</CalcRow>
+			<CalcRow label="Fuel Surcharge" result={fuelSurcharge}>
+				{unitsUsed.toFixed(1)} kWh x ${fRate}
+			</CalcRow>
+			<CalcRow label="Total Energy Cost" result={vatRes + energyFees + fuelSurcharge} highlight />
+			<CalcRow
+				label="Balance Brought Forward"
+				result={amountDue - (vatRes + energyFees + fuelSurcharge)}
+				highlight
+			/>
+		</div>
+
+		<Dialog.Footer class="mt-2 border-t pt-4">
+			<span>Amount due: </span><strong>${amountDue.toFixed(2)}</strong>
+		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
