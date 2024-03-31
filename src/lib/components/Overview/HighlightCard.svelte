efde1121c8f65d0839795760650b76572cbd55b4
sipofnightshade <gregorymcmillan96@gmail.com>
2024-03-31 11:33:05 -0400

    fixed-- HighlightCard colors and spacing ✅

--- src/lib/components/Overview/HighlightCard.svelte
+++ src/lib/components/Overview/HighlightCard.svelte
@@ -3,16 +3,21 @@
 	import CircleGauge from 'lucide-svelte/icons/circle-gauge';
 	import Zap from 'lucide-svelte/icons/zap';
 
+	import { mediaQuery } from 'svelte-legos';
+
 	export let title: 'Balance Due' | 'Energy Usage' | 'Meter Information';
+
+	const isMobile = mediaQuery('(max-width: 640px)'); // $isLargeScreen => true | false
 </script>
 
 <div
-	class="flex h-52 w-full flex-col rounded-3xl border bg-background p-7 sm:p-6 md:p-4 xl:p-5 {title ===
+	class="flex h-48 w-full flex-col rounded-3xl border bg-background p-6 sm:h-52 md:p-4 xl:p-5 {title ===
 	'Meter Information'
 		? 'gap-y-8'
-		: 'gap-y-10 md:gap-y-14 lg:gap-y-12 xl:gap-y-10'} {$$props.class}"
+		: 'gap-y-6 sm:gap-y-10 md:gap-y-14 lg:gap-y-12 xl:gap-y-10'} {$$props.class}"
 	class:text-accent-foreground={title === 'Meter Information'}
-	class:sm:bg-background={title === 'Energy Usage'}
+	class:sm:bg-background={title === 'Energy Usage' && !$isMobile}
+	class:bg-muted={title === 'Energy Usage' && $isMobile}
 	class:sm:col-span-2={title === 'Balance Due'}
 	class:md:col-span-1={title === 'Balance Due'}
 >
